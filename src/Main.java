b199f5143ec807ce7a993794af1434980b9cc995
vitaliymaksimovich <cherkasovfr@yandex.ru>
2026-03-02 22:45:03 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start project");
+        System.out.println("Start the project");
         methodUser1();
         methodUser11();
     }
